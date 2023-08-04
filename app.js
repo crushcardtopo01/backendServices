66fc9345e84e31cae6ab8aa19259883d390c305a
ctrevera-rsi <silentlv5@gmail.com>
2023-08-04 12:39:21 -0600

    test dumy answer

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT ?? 3000;
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'))
 
-app.get('*', (req, res) => {
-    res.redirect('/');
-})
+// app.get('*', (req, res) => {
+//     res.redirect('/');
+// });
+
+app.get('/dummyResponse', (req, res) => {
+    let dumyRes = [{"project":"name1","date":"02/02/2023"},{"project":"name2","date":"02/02/2023"},{"project":"name3","date":"02/02/2023"},{"project":"name3","date":"02/02/2023"},{"project":"name3","date":"02/02/2023"}]
+    res.json({ message: dumyRes });
+  });
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
